10c61b0dbc3cae65f126a37b84e798656ebd7f2f
i80287 <addmlbx@gmail.com>
2023-12-06 21:09:53 +0300

    add tasks

--- ihw3_c/stack_utils.m
+++
@@ -1,9 +0,0 @@
-.macro push(%reg)
-	addi sp sp -4
-	sw %reg (sp)
-.end_macro
-
-.macro pop(%reg)
-	lw %reg (sp)
-	addi sp sp 4
-.end_macro
--- ihw3_c/string_utils.m
+++
@@ -1,5 +0,0 @@
-.macro string_swap_words_chars(%string_label, %string_size)
-	mv a1 %string_size
-	la a0 %string_label
-	jal string_swap_words_chars
-.end_macro
