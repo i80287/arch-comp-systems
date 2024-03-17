c3c4250b0ce867f88a5a082e529e45b92f81f80c
i80287 <addmlbx@gmail.com>
2024-03-18 02:37:26 +0300

    update os ihw 1

---
+++ os_ihw1/mark9/io_handler.c
@@ -0,0 +1,146 @@
+#define _XOPEN_SOURCE 700
+
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+static const size_t TRANSFER_BUFFER_SIZE = 128 * sizeof(char);
+static const char END_OF_COMMUNICATION_CHAR = (char)128;
+static const char READER_TO_PROC_FIFO[] = "reader-to-proc.fifo";
+static const char PROC_TO_WRITER_FIFO[] = "proc-to-writer.fifo";
+
+static void print_failure_message(const char* failure_message) {
+    fprintf(stderr, "[>>>] Error: %s\n", failure_message);
+}
+
+static bool run_impl(int read_file_fd, int reader_to_proc_fd, int write_file_fd,
+                     int processor_to_writer_fd) {
+    if (read_file_fd < 0) {
+        print_failure_message("File reader: can't open input file");
+        return false;
+    }
+    if (reader_to_proc_fd < 0) {
+        print_failure_message(
+            "File reader: can't open fifo [file reader] -> [text processor]");
+        return false;
+    }
+    if (write_file_fd < 0) {
+        print_failure_message("File writer: can't open output file");
+        return false;
+    }
+    if (processor_to_writer_fd < 0) {
+        print_failure_message(
+            "File writer: can't open fifo [text processor] -> [file writer]");
+        return false;
+    }
+
+    while (true) {
+        char read_buffer[TRANSFER_BUFFER_SIZE];
+        ssize_t nbytes_read = read(read_file_fd, read_buffer, TRANSFER_BUFFER_SIZE - 1);
+        if (nbytes_read < 0) {
+            print_failure_message("File reader: can't read from the input file");
+            return false;
+        }
+
+        if (nbytes_read == 0) {
+            break;
+        }
+
+        if (write(reader_to_proc_fd, read_buffer, (size_t)nbytes_read) < 0) {
+            print_failure_message(
+                "File reader: can't send data to another process using fifo [file "
+                "reader] -> [text processor]");
+            return false;
+        }
+
+        nbytes_read = read(processor_to_writer_fd, read_buffer, TRANSFER_BUFFER_SIZE - 1);
+        if (nbytes_read < 0) {
+            print_failure_message(
+                "File writer: can't read from the fifo [text processor] -> [file "
+                "writer]");
+            return false;
+        }
+
+        if (write(write_file_fd, read_buffer, (size_t)nbytes_read) < 0) {
+            print_failure_message("File writer: can't write to output file");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static void notify_end_of_communication(int reader_to_proc_fd) {
+    if (reader_to_proc_fd >= 0) {
+        const char end_message[] = { END_OF_COMMUNICATION_CHAR };
+        write(reader_to_proc_fd, end_message, sizeof(end_message));
+    }
+}
+
+static bool run(const char* input_filename, const char* output_filename) {
+    umask(0);
+    mknod(READER_TO_PROC_FIFO, S_IFIFO | 0666, 0);
+    umask(0);
+    mknod(PROC_TO_WRITER_FIFO, S_IFIFO | 0666, 0);
+
+    int read_file_fd           = open(input_filename, O_RDONLY);
+    int reader_to_proc_fd      = open(READER_TO_PROC_FIFO, O_WRONLY);
+    int write_file_fd          = open(output_filename, O_WRONLY | O_CREAT, 0644);
+    int processor_to_writer_fd = open(PROC_TO_WRITER_FIFO, O_RDONLY);
+    bool result = run_impl(read_file_fd, reader_to_proc_fd, write_file_fd, processor_to_writer_fd);
+    // Send message about end of communication between processes
+    notify_end_of_communication(reader_to_proc_fd);
+
+    if (read_file_fd >= 0) {
+        if (close(read_file_fd) < 0) {
+            print_failure_message("File reader: can't close opened input file");
+            result = false;
+        }
+    }
+    if (reader_to_proc_fd >= 0) {
+        if (close(reader_to_proc_fd) < 0) {
+            print_failure_message(
+                "File reader: can't close writing side of the fifo [file reader] "
+                "-> "
+                "[text processor]");
+            result = false;
+        }
+    }
+    if (write_file_fd >= 0) {
+        if (close(write_file_fd) < 0) {
+            print_failure_message("File writer: can't close opened output file");
+            result = false;
+        }
+    }
+    if (processor_to_writer_fd >= 0) {
+        if (close(processor_to_writer_fd) < 0) {
+            print_failure_message(
+                "File writer: can't close opened fifo [text processor] -> [file "
+                "writer]");
+            result = false;
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, const char* const argv[]) {
+    if (argc != 3) {
+        fputs(
+            "[>>>] Error        : expeted 2 arguments\n"
+            "[>>>] Usage hint   : executable input-file-name output-file-name\n"
+            "[>>>] Usage example: main.out in.txt out.txt\n",
+            stderr);
+        return EXIT_FAILURE;
+    }
+
+    bool result = run(argv[1], argv[2]);
+    return result ? EXIT_SUCCESS : EXIT_FAILURE;
+}
---
+++ os_ihw1/mark9/text_processor.c
@@ -0,0 +1,146 @@
+#define _XOPEN_SOURCE 700
+
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "parser.h"
+
+static const size_t TRANSFER_BUFFER_SIZE = 128 * sizeof(char);
+static const char END_OF_COMMUNICATION_CHAR = (char)128;
+static const char READER_TO_PROC_FIFO[]  = "reader-to-proc.fifo";
+static const char PROC_TO_WRITER_FIFO[]  = "proc-to-writer.fifo";
+
+static void print_failure_message(const char* failure_message) {
+    fprintf(stderr, "[>>>] Error: %s\n", failure_message);
+}
+
+static void swap(unsigned char* ustring, size_t l, size_t r) {
+    unsigned char cl = ustring[l];
+    ustring[l]       = ustring[r];
+    ustring[r]       = cl;
+}
+
+static void reverse_substring(char* string, size_t l, size_t r) {
+    assert(l <= r);
+    unsigned char* ustring = (unsigned char*)string;
+    while (l < r) {
+        swap(ustring, l, r);
+        l++;
+        r--;
+    }
+}
+
+static size_t min(size_t a, size_t b) { return a <= b ? a : b; }
+
+static size_t max(size_t a, size_t b) { return a >= b ? a : b; }
+
+static bool run_impl(uint32_t start_n, uint32_t end_n, int reader_to_proc_fifo_fd,
+                     int proc_to_writer_fifo_fd) {
+    if (reader_to_proc_fifo_fd < 0) {
+        print_failure_message(
+            "Data processor: can't open fifo [file reader] -> [text processor]");
+        return false;
+    }
+    if (proc_to_writer_fifo_fd < 0) {
+        print_failure_message(
+            "Data processor: can't open fifo [text processor] -> [file writer]");
+        return false;
+    }
+
+    for (size_t current_index = 0;;) {
+        char read_buffer[TRANSFER_BUFFER_SIZE];
+        ssize_t nbytes_read =
+            read(reader_to_proc_fifo_fd, read_buffer, TRANSFER_BUFFER_SIZE - 1);
+        if (nbytes_read < 0) {
+            print_failure_message(
+                "Data processor: can't read from fifo [file reader] -> [text "
+                "processor]");
+            return false;
+        }
+        if (nbytes_read == 0) {
+            break;
+        }
+
+
+        size_t string_length = (size_t)nbytes_read;
+        bool end_of_communication = read_buffer[string_length] == END_OF_COMMUNICATION_CHAR;
+        if (end_of_communication) {
+            break;
+        }
+        
+        if (current_index <= start_n && start_n < current_index + string_length) {
+            size_t l = start_n - current_index;
+            size_t r = min(end_n, current_index + string_length - 1);
+            reverse_substring(read_buffer, l, r);
+        } else if (current_index <= end_n && end_n < current_index + string_length) {
+            size_t l = max(start_n, current_index) - current_index;
+            size_t r = end_n - current_index;
+            reverse_substring(read_buffer, l, r);
+        }
+        current_index += string_length;
+
+        if (write(proc_to_writer_fifo_fd, read_buffer, string_length) < 0) {
+            print_failure_message(
+                "Data processor: can't send data to another process using fifo [text "
+                "processor] -> [file writer]");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static bool run(parse_result_t parse_result) {
+    umask(0);
+    mknod(READER_TO_PROC_FIFO, S_IFIFO | 0666, 0);
+    umask(0);
+    mknod(PROC_TO_WRITER_FIFO, S_IFIFO | 0666, 0);
+
+    uint32_t start_n = parse_result.start_n1;
+    uint32_t end_n   = parse_result.end_n2;
+    assert(start_n <= end_n);
+    int reader_to_proc_fifo_fd = open(READER_TO_PROC_FIFO, O_RDONLY);
+    int proc_to_writer_fifo_fd = open(PROC_TO_WRITER_FIFO, O_WRONLY);
+    bool result =
+        run_impl(start_n, end_n, reader_to_proc_fifo_fd, proc_to_writer_fifo_fd);
+    if (reader_to_proc_fifo_fd >= 0) {
+        if (close(reader_to_proc_fifo_fd) < 0) {
+            print_failure_message(
+                "Data processor: can't open close opened fifo  [file reader] -> "
+                "[text "
+                "processor]");
+            result = false;
+        }
+    }
+    if (proc_to_writer_fifo_fd >= 0) {
+        if (close(proc_to_writer_fifo_fd) < 0) {
+            print_failure_message(
+                "Data processor: can't open close opened fifo [text processor] -> "
+                "[file writer]");
+            result = false;
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, const char* const argv[]) {
+    parse_result_t parse_result = parse_arguments(argc, argv);
+    if (parse_result.status != PARSE_OK) {
+        handle_parsing_error(parse_result.status);
+        print_failure_message("Incorrect arguments provided");
+        return EXIT_FAILURE;
+    }
+
+    bool result = run(parse_result);
+    return result ? EXIT_SUCCESS : EXIT_FAILURE;
+}
