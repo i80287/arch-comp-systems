fab4dea1c4ee5b04aa58c9e28bca8c4644667226
i80287 <addmlbx@gmail.com>
2024-05-21 05:59:25 +0300

    update os ihw 3

--- os_ihw3/mark-10/compile.sh
+++ os_ihw3/mark-10/compile.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-gcc ./net/first-worker.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o first-worker
-gcc ./net/second-worker.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o second-worker
-gcc ./net/third-worker.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o third-worker
-gcc ./net/logs-collector.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o logs-collector
-gcc ./net/manager.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o manager
-gcc ./net/server.c ./net/server-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lpthread -o server
+gcc ./net/first-worker.c ./net/client-tools.c ./util/parser.c -g3 -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o first-worker
+gcc ./net/second-worker.c ./net/client-tools.c ./util/parser.c -g3 -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o second-worker
+gcc ./net/third-worker.c ./net/client-tools.c ./util/parser.c -g3 -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o third-worker
+gcc ./net/logs-collector.c ./net/client-tools.c ./util/parser.c -g3 -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o logs-collector
+gcc ./net/manager.c ./net/client-tools.c ./util/parser.c -g3 -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o manager
+gcc ./net/server.c ./net/server-tools.c ./util/parser.c -g3 -O2 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lpthread -o server
--- os_ihw3/mark-6-7/compile.sh
+++ os_ihw3/mark-6-7/compile.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-gcc ./net/first-worker.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o first-worker
-gcc ./net/second-worker.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o second-worker
-gcc ./net/third-worker.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o third-worker
-gcc ./net/logs-collector.c ./net/client-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o logs-collector
-gcc ./net/server.c ./net/server-tools.c ./util/parser.c -g3 -fsanitize=address,undefined -D_FORTIFY_SOURCE=3 -fstack-protector-all -mshstk -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lpthread -o server
+gcc ./net/first-worker.c ./net/client-tools.c ./util/parser.c -g3 -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o first-worker
+gcc ./net/second-worker.c ./net/client-tools.c ./util/parser.c -g3 -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o second-worker
+gcc ./net/third-worker.c ./net/client-tools.c ./util/parser.c -g3 -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o third-worker
+gcc ./net/logs-collector.c ./net/client-tools.c ./util/parser.c -g3 -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lm -o logs-collector
+gcc ./net/server.c ./net/server-tools.c ./util/parser.c -g3 -O1 -Wall -Wextra -Wpedantic -Wsign-conversion -lrt -lpthread -o server
